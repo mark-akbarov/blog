68eb2d43676d416d78220536f7d6613b2c3177fc
mark-akbarov <mark-akbarov@gmail.com>
2021-12-07 20:27:52 +0500

    Add Pagination

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,11 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import  LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, DetailView, 
     CreateView, UpdateView, 
     DeleteView)
+from django.contrib.auth.models import User 
 from .models import Post
 
 
@@ -20,7 +21,20 @@ class PostListView(ListView):
     template_name = 'blogs/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blogs/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
