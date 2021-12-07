9dd48c7e00fb591c3d45ae22b47694852c684222
mark-akbarov <mark-akbarov@gmail.com>
2021-12-07 14:56:48 +0500

    Add Create and Update Profile

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib.auth import views as auth_view
 from users import views as user_view
 
@@ -26,3 +30,7 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blogs.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
